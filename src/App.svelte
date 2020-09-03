b353293589e1b0be6c3affc58d1b25ca61d83ebc
Jacavena <jacavena@github.com>
2020-09-03 18:44:02 +0800

    Use github API to get projects

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,47 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+
+	let repos = [];
+
+	onMount(async () => {
+		const apiKey = '5c0031db45540faf979d3d0b9c46d24a13d903ec'; // Access Public Repositories API Key
+		const query = `{
+			viewer {
+				repositories(first: 25, orderBy: {direction: DESC, field: CREATED_AT}) {
+					nodes {
+						name
+						description
+						url
+					}
+				}
+			}
+		}`;
+
+		const res = await fetch('https://api.github.com/graphql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+				'Authorization': 'bearer ' + apiKey
+			},
+			body: JSON.stringify({ query })
+		})
+
+		const data = await res.json();
+		repos = data.data.viewer.repositories.nodes;
+		console.log(repos);
+	});
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#each repos as repo}
+		<div>
+			<h2><a href={repo.url}>{repo.name}</a></h2>
+			<h3>{repo.description}</h3>
+		</div>
+	{:else}
+		<p>Loading repositories...</p>
+	{/each}
 </main>
 
 <style>
