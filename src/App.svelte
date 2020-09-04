77b8af72ed028af0f72b449a67746019cff20cb4
Jacavena <jacavena@github.com>
2020-09-04 18:34:30 +0800

    Add design to website

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,22 @@
 	let repos = [];
 
 	onMount(async () => {
-
 		const res = await fetch('https://api.github.com/users/jacavena/repos', {
 			headers: {
 				'Accept': 'application/vnd.github.v3+json'
 			}
 		})
 
-		repos = await res.json();
+		const data = await res.json();
+		repos = data.reverse();
 	});
 </script>
 
 <main>
 	{#each repos as repo}
-		<div>
-			<h2><a href={repo.url}>{repo.name}</a></h2>
-			<h3>{repo.description}</h3>
+		<div class="repo">
+			<h2><a href={repo.html_url}>{repo.name}</a></h2>
+			<p>{repo.description}</p>
 		</div>
 	{:else}
 		<p>Loading repositories...</p>
@@ -28,22 +28,35 @@
 
 <style>
 	main {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	main > p {
+		font-size: 2rem;
+		padding-top: 4rem;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.repo {
+		border: 1px solid #ddd;
+		border-radius: 5px;
+		box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+		padding: 32px;
+		margin: 0 16px;
+		margin-top: 36px;
+	}
+
+	.repo h2 {
+		margin: 0;
+		font-size: 1.75rem;
+	}
+
+	.repo p {
+		margin-top: 16px;
+		margin-bottom: 0;
+		font-size: 1.2rem;
 	}
 </style>
